c5b08c25c5f77992f1c9f8d4f2b6b235abfbda8f
Julian Scheffers <julian@scheffers.net>
2024-03-11 16:17:01 +0100

    Bus definition parsing

--- bustool.py
+++ bustool.py
@@ -7,41 +7,7 @@ def reflect_repr(instance):
     """__repr__() implementation for classes that directly set their member variables from constructor parameters."""
     code     = type(instance).__init__.__code__
     argnames = code.co_varnames[1:code.co_argcount]
-    return type(instance).__name__ + "(" + ", ".join(str(getattr(instance, x)) for x in argnames) + ")"
-
-
-class Span:
-    __repr__ = reflect_repr
-    def __init__(self, msb: int, lsb: int):
-        if msb < lsb:
-            raise ValueError("MSB should be higher than LSB")
-        self.msb   = msb
-        self.lsb   = lsb
-        self.width = msb - lsb + 1
-    
-    @staticmethod
-    def parse(raw):
-        if '-' in raw:
-            s = raw.split('-')
-            return Span(int(s[0]), int(s[1]))
-        else:
-            v = int(raw)
-            return Span(v, v)
-    
-    @staticmethod
-    def default():
-        return Span(0, 0)
-
-
-class Parameter:
-    __repr__ = reflect_repr
-    def __init__(self, desc: str, default: str):
-        self.desc    = desc
-        self.default = default
-    
-    @staticmethod
-    def parse(raw):
-        return Parameter(raw["desc"] if "desc" in raw else None, raw["default"])
+    return type(instance).__name__ + "(" + ", ".join(repr(getattr(instance, x)) for x in argnames) + ")"
 
 
 class Operator:
@@ -115,13 +81,20 @@ operators = {
 
 
 class Expression:
-    __repr__ = reflect_repr
     def __init__(self, typ: str|Operator, args: list|int|str):
         self.typ        = typ
         self.args       = args
         self.precedence = 11
-        if type(typ) is Operator:
+        if typ == "var":
+            self.vars = {args: args}
+        elif type(typ) is Operator:
             self.precedence = self.typ.precedence
+            self.vars = {}
+            for x in self.args:
+                for var in x.vars:
+                    self.vars[var] = var
+        else:
+            self.vars = {}
     
     @staticmethod
     def parse(raw):
@@ -168,10 +141,141 @@ class Expression:
                 tmp[i] = "(" + tmp[i] + ")"
         
         return self.typ.build(tmp)
+    
+    def __repr__(self):
+        return self.build(self.vars)
 
 
-f    = open("test/expr.yml", "r")
-raw  = yaml.safe_load(f)
-x    = Expression.parse(raw)
-vars = {"is_ok": "is.ok", "is_fine": "is.fine"}
-print(x.build(vars))
+class Parameter:
+    __repr__ = reflect_repr
+    def __init__(self, desc: str, default: Expression):
+        self.desc    = desc
+        self.default = default
+    
+    @staticmethod
+    def parse(raw):
+        return Parameter(raw["desc"] if "desc" in raw else None, Expression.parse(raw["default"]))
+
+
+class Span:
+    __repr__ = reflect_repr
+    def __init__(self, msb: Expression, lsb: Expression):
+        self.msb   = msb
+        self.lsb   = lsb
+    
+    @staticmethod
+    def parse(raw):
+        if type(raw) in [list, tuple]:
+            return Span(Expression.parse(raw[1]), Expression.parse(raw[0]))
+        elif type(raw) is str and '-' in raw:
+            s = raw.split('-')
+            return Span(int(s[0]), int(s[1]))
+        else:
+            return Span(Expression.parse({"$sub": [raw, 1]}), Expression("const", 0))
+    
+    @staticmethod
+    def default():
+        return Span(0, 0)
+
+
+class ClockSpec:
+    __repr__ = reflect_repr
+    def __init__(self, typ: str, sigid: str, rising: bool):
+        self.typ    = typ
+        self.sigid  = sigid
+        self.rising = rising
+    
+    @staticmethod
+    def parse(raw):
+        if type(raw) is not dict:
+            raise ValueError("Invalid clock specification")
+        if raw["edge"] not in ["rising", "falling"]:
+            raise ValueError("Invalid clock edge")
+        if raw["type"] not in ["ext_clock", "bus_clock"]:
+            raise ValueError("Invalid clock type")
+        return ClockSpec(raw["type"], raw["signal"], raw["edge"] == "rising")
+    
+    def build(self, ref, parent):
+        return self.sigid if self.typ == "ext_clk" else parent.build_sigref(ref, self.sigid)
+
+
+class TransSpec:
+    __repr__ = reflect_repr
+    def __init__(self, request: Expression, accept: Expression, stall: Expression):
+        self.request = request
+        self.accept  = accept
+        self.stall   = stall
+    
+    @staticmethod
+    def parse(raw):
+        if type(raw) is not dict:
+            raise ValueError("Invalid transaction specification")
+        return TransSpec(
+            Expression.parse(raw["request"]),
+            Expression.parse(raw["accept"]) if "accept" in raw else Expression("const", 1),
+            Expression.parse(raw["stall"])  if "stall"  in raw else Expression("const", 0),
+        )
+
+
+class Signal:
+    __repr__ = reflect_repr
+    def __init__(self, desc: str, span: Span, time: Expression):
+        self.desc = desc
+        self.span = span
+        self.time = time
+    
+    @staticmethod
+    def parse(raw):
+        if raw == None:
+            return Signal.default()
+        else:
+            return Signal(
+                raw["desc"] if "desc" in raw else None,
+                Span.parse(raw["span"]) if "span" in raw else Span.default(),
+                Expression.parse(raw["time"]) if "time" in raw else Expression("const", 0)
+            )
+    
+    @staticmethod
+    def default():
+        return Signal(None, Span.default(), Expression("const", 0))
+
+
+class AsymmetricBus:
+    __repr__ = reflect_repr
+    def __init__(self, desc: str, ctl: str, dev: str, params: list[Parameter], trans: TransSpec, clk: ClockSpec, outs: list[Signal], ins: list[Signal]):
+        self.desc   = desc
+        self.ctl    = ctl
+        self.dev    = dev
+        self.params = params
+        self.trans  = trans
+        self.clk    = clk
+        self.outs   = outs
+        self.ins    = ins
+    
+    @staticmethod
+    def parse(raw: dict):
+        params = {}
+        if "parameters" in raw:
+            for key in raw["parameters"]:
+                params[key] = Parameter.parse(raw["parameters"][key])
+        outs = {}
+        for key in raw["outputs"]:
+            outs[key] = Signal.parse(raw["outputs"][key])
+        ins = {}
+        for key in raw["inputs"]:
+            ins[key] = Signal.parse(raw["inputs"][key])
+        return AsymmetricBus(
+            raw["desc"] if "desc" in raw else None,
+            raw["controller"],
+            raw["device"],
+            params,
+            TransSpec.parse(raw["transaction"]),
+            ClockSpec.parse(raw["clock"]),
+            outs,
+            ins
+        )
+
+
+f   = open("test/bus.yml")
+raw = yaml.safe_load(f)
+print(AsymmetricBus.parse(raw["bus_a"]))
