116c33dc99686cb5afa2b0396a77476ad909d70a
RobotMan2412 <julian@scheffers.net>
2024-03-14 14:41:45 +0100

    ok

--- sysverilog.py
+++ sysverilog.py
@@ -1,6 +1,23 @@
 
-def o_sqrt(x):  return "$sqrt" + x
-def o_clog2(x): return "$clog2" + x
+import parser, builder
+
+def o_sqrt(x):  return "$sqrt(" + x + ")"
+def o_clog2(x): return "$clog2(" + x + ")"
 def o_not(x):   return "!" + x
 def o_and(x):   return "&&" + x
 def o_or(x):    return "||" + x
+
+def line_comment(writer: builder.Writer, text: str):
+    for line in text.splitlines():
+        writer.line("// " + line)
+
+def build_intf(writer: builder.Writer, id: str, bus: parser.AsymmetricBus):
+    if bus.desc: line_comment(writer, bus.desc)
+    if bus.clk.typ == "bus_clock":
+        writer.line(f"interface {id}(")
+        writer.pushIndent()
+        writer.popIndent()
+        writer.line(");")
+    else:
+        writer.line(f"interface {id};")
+    writer.line("endinterface")
